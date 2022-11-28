11288135d5c1c7e84195e21123e1861c95e08fdb
Tuncay YAYLALI <81981206+tuncayyaylali@users.noreply.github.com>
2022-11-28 15:29:57 +0300

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oat Meal')
 streamlit.text('Kale & Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
