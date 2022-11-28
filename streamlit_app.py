a39e64b1c7bb876ef3fcdb9aab2ffab3b6fb4352
Tuncay YAYLALI <81981206+tuncayyaylali@users.noreply.github.com>
2022-11-28 15:29:00 +0300

    some symbols added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oat Meal')
 streamlit.text('Kale & Spinach & Rocket Smoothie')
